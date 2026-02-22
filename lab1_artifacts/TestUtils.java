40769e6ae6190487cd99d4026dc331d90b4791f2
40374429-Dawar-Mohammadi <dmohammadi01@qub.ac.uk>
2026-02-22 16:09:41 +0000

    refactor(TODO3): extract printTestPassed/Failed into TestUtils
    Add reusable pass fail print methods to TestUtils class.
    Replace all inline System.out.println calls in UserTest with
    TestUtils.printTestPassed printTestFailed for cleaner output.

--- lab1_artifacts/TestUtils.java
+++ lab1_artifacts/TestUtils.java
@@ -1,9 +1,20 @@
 package utils;
 
+/**
+ * Utility class for test automation helper methods.
+ * Provides colored pass/fail output for consistent test reporting.
+ */
 public class TestUtils {
-	
-	public static final String TEXT_COLOR_GREEN = "\u001B[32m"; // from https://www.geeksforgeeks.org/how-to-print-colored-text-in-java-console/
-    public static final String TEXT_COLOR_RED = "\u001B[31m"; 
-    public static final String TEXT_COLOR_RESET = "\u001B[0m"; 
-	
+    public static final String TEXT_COLOR_GREEN = "\u001B[32m";
+    public static final String TEXT_COLOR_RED = "\u001B[31m";
+    public static final String TEXT_COLOR_RESET = "\u001B[0m";
+
+    // TODO3: refactored println pass/fail into reusable methods
+    public static void printTestPassed(String test_case_name) {
+        System.out.println(TEXT_COLOR_GREEN + "PASSED -- " + test_case_name + TEXT_COLOR_RESET);
+    }
+
+    public static void printTestFailed(String test_case_name) {
+        System.out.println(TEXT_COLOR_RED + "FAILED -- " + test_case_name + TEXT_COLOR_RESET);
+    }
 }
--- lab1_artifacts/UserTest.java
+++ lab1_artifacts/UserTest.java
@@ -20,30 +20,31 @@ public class UserTest {
         // 3-Verify
         System.out.println("Starting the assertions of the test method: testUserConstructor");
 
+        // if-else verification
         if (testUser.getUsername() == test_username)
-            System.out.println(TestUtils.TEXT_COLOR_GREEN + "TC1-getUsername-Passed" + TestUtils.TEXT_COLOR_RESET);
+            TestUtils.printTestPassed("TC1-getUsername");
         else
-            System.out.println(TestUtils.TEXT_COLOR_RED + "TC1-getUsername-FAILED" + TestUtils.TEXT_COLOR_RESET);
+            TestUtils.printTestFailed("TC1-getUsername");
 
         if (testUser.getPassword() == test_password)
-            System.out.println(TestUtils.TEXT_COLOR_GREEN + "TC2-getPassword-Passed" + TestUtils.TEXT_COLOR_RESET);
+            TestUtils.printTestPassed("TC2-getPassword");
         else
-            System.out.println(TestUtils.TEXT_COLOR_RED + "TC2-getPassword-FAILED" + TestUtils.TEXT_COLOR_RESET);
+            TestUtils.printTestFailed("TC2-getPassword");
 
         if (testUser.getFirst_name() == test_first_name)
-            System.out.println(TestUtils.TEXT_COLOR_GREEN + "TC3-getFirst_name-Passed" + TestUtils.TEXT_COLOR_RESET);
+            TestUtils.printTestPassed("TC3-getFirst_name");
         else
-            System.out.println(TestUtils.TEXT_COLOR_RED + "TC3-getFirst_name-FAILED" + TestUtils.TEXT_COLOR_RESET);
+            TestUtils.printTestFailed("TC3-getFirst_name");
 
         if (testUser.getLast_name() == test_last_name)
-            System.out.println(TestUtils.TEXT_COLOR_GREEN + "TC4-getLast_name-Passed" + TestUtils.TEXT_COLOR_RESET);
+            TestUtils.printTestPassed("TC4-getLast_name");
         else
-            System.out.println(TestUtils.TEXT_COLOR_RED + "TC4-getLast_name-FAILED" + TestUtils.TEXT_COLOR_RESET);
+            TestUtils.printTestFailed("TC4-getLast_name");
 
         if (testUser.getMobile_number() == test_mobile_number)
-            System.out.println(TestUtils.TEXT_COLOR_GREEN + "TC5-getMobile_number-Passed" + TestUtils.TEXT_COLOR_RESET);
+            TestUtils.printTestPassed("TC5-getMobile_number");
         else
-            System.out.println(TestUtils.TEXT_COLOR_RED + "TC5-getMobile_number-FAILED" + TestUtils.TEXT_COLOR_RESET);
+            TestUtils.printTestFailed("TC5-getMobile_number");
 
         // 4-Teardown: none
     }
