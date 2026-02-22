38a4ca529118433b28f562de2a951f23e3b23bea
Shane <shanedort13447@gmail.com>
2026-02-22 23:47:51 +0000

    refactor: move account loading into AccountController

--- lab1_artifacts/AccountController.java
+++ lab1_artifacts/AccountController.java
@@ -1,5 +1,7 @@
 package controller;
 
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
 import java.util.Vector;
 
 import model.Account;
@@ -25,5 +27,24 @@ public class AccountController {
 			}
 		}
 		return false;
+	}
+
+	public static void loadAccountData(Vector<Account> accounts) {
+		Account anAccount;
+		try {
+			anAccount = new Account("5495-1234", "mike", "Standard", new SimpleDateFormat("dd/MM/yyyy").parse("20/08/2019"));
+			accounts.add(anAccount);
+
+			anAccount = new Account("5495-1239", "mike", "Standard", new SimpleDateFormat("dd/MM/yyyy").parse("20/08/2020"));
+			accounts.add(anAccount);
+
+			anAccount = new Account("5495-1291", "mike", "Saving", new SimpleDateFormat("dd/MM/yyyy").parse("21/07/2019"));
+			accounts.add(anAccount);
+
+			anAccount = new Account("5495-6789", "David.McDonald@gmail.com", "Saving", new SimpleDateFormat("dd/MM/yyyy").parse("20/08/2019"));
+			accounts.add(anAccount);
+		} catch (ParseException e) {
+			e.printStackTrace();
+		}
 	}
 }
