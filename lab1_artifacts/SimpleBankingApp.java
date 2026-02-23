53c014050f95d4a261786159909f6486ee015c24
Shane <shanedort13447@gmail.com>
2026-02-23 00:10:51 +0000

    refactor: delegate account loading to AccountController

--- lab1_artifacts/SimpleBankingApp.java
+++ lab1_artifacts/SimpleBankingApp.java
@@ -32,27 +32,7 @@ public class SimpleBankingApp {
 	}
 	
 	public static void loadAccountData()  {
-		// structure of each record: 
-		// account number, username (email) of account holder, account type (Standard or Saving), account_opening_date
-
-		// in the ideal case, we will read from file or database, but let's hard-code for now
-		Account anAccount;
-		try {
-			anAccount = new Account("5495-1234", "mike", "Standard", new SimpleDateFormat("dd/MM/yyyy").parse("20/08/2019"));
-			accounts.add(anAccount);
-			
-			anAccount = new Account("5495-1239", "mike", "Standard", new SimpleDateFormat("dd/MM/yyyy").parse("20/08/2020"));
-			accounts.add(anAccount);
-
-			anAccount = new Account("5495-1291", "mike", "Saving", new SimpleDateFormat("dd/MM/yyyy").parse("21/07/2019"));
-			accounts.add(anAccount);
-
-			anAccount = new Account("5495-6789", "David.McDonald@gmail.com", "Saving", new SimpleDateFormat("dd/MM/yyyy").parse("20/08/2019"));
-			accounts.add(anAccount);
-
-		} catch (ParseException e) {			
-			e.printStackTrace();
-		}  
+		AccountController.loadAccountData(accounts);
 	}
 	
 	public static void printAllAccounts() {
